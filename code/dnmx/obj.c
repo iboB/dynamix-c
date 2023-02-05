6f34800a54eeecd2f5f05b9492985d9da0ce54f2
Borislav Stanimirov <b.stanimirov@abv.bg>
2023-02-05 12:44:09 +0200

    Complete uninitialized_prepare_data

--- code/dnmx/obj.c
+++ code/dnmx/obj.c
@@ -11,7 +11,7 @@
 static const dnmx_obj_type EMPTY_OBJ_TYPE;
 const dnmx_obj_type* const DNMX_NULL_OBJ_TYPE = &EMPTY_OBJ_TYPE;
 
-// used by objects with no mixin data, so they would return nullptr on get_mixin
+// used by objects with no mixin data, so they would return NULL on get_mixin
 // without the need to check or crash
 static const dnmx_obj_mixin_data EMPTY_MIXIN_DATA;
 
@@ -35,15 +35,25 @@ static dnmx_obj_mixin_data* uninitialized_prepare_data(const dnmx_obj* obj, cons
 
     for (uint32_t i = 0; i < new_type->num_mixins; ++i) {
         const dnmx_mixin_type_info* new_info = old_type->mixins[i];
-        dnmx_mixin_id id = new_info->id;
-        uint32_t new_index = new_type->sparse_mixin_indices[id];
+        const dnmx_mixin_id id = new_info->id;
+        const uint32_t new_index = new_type->sparse_mixin_indices[id];
         if (dnmx_obj_type_has_mixin(old_type, id)) {
             // mixin is in both types
-            uint32_t old_index = old_type->sparse_mixin_indices[id];
+            const uint32_t old_index = old_type->sparse_mixin_indices[id];
             new_mixin_data[new_index] = old_mixin_data[old_index]; // redirect
         }
         else {
             // mixin is in new type only, so allocate buffer
+            const size_t buf_size = dnmx_size_for_mixin_data_buf(new_info->size, new_info->alignment);
+            uint8_t* mixin_buf = (uint8_t*)malloc(buf_size);
+            if (!mixin_buf) {
+                // out of mem
+                // TODO: clear all allocated previously and return NULL
+                return NULL;
+            }
+            const size_t mixin_off = dnmx_mixin_offset_in_data_buf(mixin_buf, new_info->alignment);
+            new_mixin_data[new_index].buffer = mixin_buf;
+            new_mixin_data[new_index].mixin = mixin_buf + mixin_off;
         }
     }
 
